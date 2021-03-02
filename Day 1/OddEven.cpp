6209d4695fcbb531d7f2d406595638e1f47b84ba
Shivlal <sshivlal9601@gmail.com>
2021-03-02 13:22:00 +0530

    C++ code for Odd and Even problem

---
+++ Day 1/OddEven.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int num;
+    cin >> num;
+
+    if (num % 2 == 0)
+    {
+        cout << num << " is even!" << endl;
+    }
+    else
+    {
+        cout << num << " is odd!" << endl;
+    }
+}
