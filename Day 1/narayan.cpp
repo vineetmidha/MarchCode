8504b895fe13c44c0735e04ca13390aef7aaadd4
NarayanBavisetti <narayan3119@gmail.com>
2021-03-02 14:48:47 +0530

    udate narayan.cpp file

--- Day 1/narayan.cpp
+++ Day 1/narayan.cpp
@@ -1,9 +1,11 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main(){
     int a;
+
     cin>>a;
     if(a %2== 0 )
+    
     cout<<a<<"is even number";
     else
     cout<<a<<"is an odd number";
