2d41b79f01dda55a7e23d1b24a2d2372eddaffc4
vineet_midha <vineet.midha@yahoo.com>
2021-03-03 21:14:52 +0530

    Day 1 vineetmidha.cpp

---
+++ Day 2/vineetmidha.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int n;
+    cin >> n;
+
+    if (n%5==0 && n%11==0)
+        cout<<"Divisible";
+    else
+        cout<<"Not Divisible";
+
+    return 0;
+}
